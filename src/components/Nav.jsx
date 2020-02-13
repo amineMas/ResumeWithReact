7d9f7ade3561c6a3a4f66e4412dfd5f1230ec1b1
Amine <aminemastouri@gmail.com>
2020-02-13 13:11:26 +0100

    component Navbar added

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+  <a className="navbar-brand js-scroll-trigger" href="#page-top">
+    <span className="d-block d-lg-none">Clarence Taylor</span>
+    <span className="d-none d-lg-block">
+      <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/avatar.png" alt="image-profil" />
+    </span>
+  </a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon" />
+  </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="#about">Qui suis-je</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="#experience">Mes expériences</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="#education">Diplômes/Formations</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="#skills">Skills</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    )
+  }
+}
