f29d3caf790c8975a870370c79f7d9c941456c03
Amine <aminemastouri@gmail.com>
2020-02-16 00:29:19 +0100

    Updates

---
+++ precache-manifest.99116c6ed917cd69e7916c07f1b9f04a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838a50c1b8bb4a1798b849a2b042cbb9",
+    "url": "/ResumeWithReact/index.html"
+  },
+  {
+    "revision": "223ef8851d8b3e84ba32",
+    "url": "/ResumeWithReact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dfc558704de45d560005",
+    "url": "/ResumeWithReact/static/js/2.c39f3270.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ResumeWithReact/static/js/2.c39f3270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223ef8851d8b3e84ba32",
+    "url": "/ResumeWithReact/static/js/main.264be254.chunk.js"
+  },
+  {
+    "revision": "92cfe85898d4dd5e6fd3",
+    "url": "/ResumeWithReact/static/js/runtime-main.0ac77157.js"
+  }
+]);
